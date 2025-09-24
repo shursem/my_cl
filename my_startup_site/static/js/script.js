ca9857cbf64782d6d6f12de50613d4dfadab0b5c
shursem <71173837+shursem@users.noreply.github.com>
2025-09-24 22:29:18 +0530

    Delete my_startup_site directory

--- my_startup_site/static/js/script.js
+++
@@ -1,5 +0,0 @@
-const toggle = document.getElementById("theme-toggle");
-toggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-    document.body.classList.toggle("light-mode");
-});
